78978842f86a5f4142c11792bbcd5e52f2665a87
David JM <6676074+biodavidjm@users.noreply.github.com>
2026-02-18 00:23:00 +0100

    Update plot_feature_heatmap.R

--- R/plot_feature_heatmap.R
+++ R/plot_feature_heatmap.R
@@ -53,7 +53,8 @@
 #' @importFrom grDevices dev.off
 #' @importFrom tibble column_to_rownames
 #' @importFrom tidyr pivot_wider
-#' @importFrom grid grid.text
+#' @importFrom grid grid.text convertUnit convertWidth convertHeight
+#'   grobWidth grobHeight textGrob unit gpar
 #' @examples
 #' \dontrun{
 #' plot_feature_heatmap(set_id = "11725",
